f550b481ac376a6f68ef5d0013a89cf1569c65c2
Shrimp-Chip <esample21@gmail.com>
2023-08-14 12:57:04 -0400

    Renamed Documentation Folder

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -12,14 +12,13 @@ cd .vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Evan-Bertis-Sample/Curly.git main:gh-pages
+git push -f https://github.com/Evan-Bertis-Sample/Curly.git main:gh-pages
 
 cd -
